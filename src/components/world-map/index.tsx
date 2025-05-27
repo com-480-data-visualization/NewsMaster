c0be52ccdeb637b0c8c23f5bd2988e284bd53207
fdejean <florian.dejean@epfl.ch>
2025-05-27 20:21:03 +0200

    fix: build error

--- src/components/world-map/index.tsx
+++ src/components/world-map/index.tsx
@@ -2,7 +2,6 @@
 export { default as WorldMap } from './WorldMap';
 export { default as WorldMapPage } from './WorldMapPage';
 export { default as CountryTooltip } from './CountryTooltip';
-export { default as CountryInfoPanel } from './CountryInfoPanel';
+
 export { default as GlobalRankings } from './GlobalRankings';
 export { ImportExportToggle } from './ImportExportToggle';
-export { default as Legend } from './Legend'; 
